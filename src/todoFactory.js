bd84054d74d62d5611063967124aeed8a65df7f6
Raza Hassan <bilgramiraza@yahoo.com>
2021-10-12 11:59:58 +0530

    Added Deletion Operations and Error Handling

--- src/todoFactory.js
+++ src/todoFactory.js
@@ -1,14 +1,10 @@
 class TodoItem{
-    constructor(project, title, dueDate, priority, done, description){
-        this._project = project;
+    constructor(title, dueDate, priority, done, description){
         this._title = title;
         this._dueDate = new Date(dueDate);
         this._priority = priority;
         this._done = done;
         this._description = description;
-    }
-    get project(){
-        return this._project;
     }
     get title(){
         return this._title;
@@ -26,7 +22,7 @@ class TodoItem{
         return this._description;
     }
     get display(){
-       return [this._project, this._title, this._dueDate.toISOString().substring(0,10), this._priority, this._done, this._description];
+       return [this._title, this._dueDate.toISOString().substring(0,10), this._priority, this._done, this._description];
     }
 }
 
@@ -34,20 +30,57 @@ class ToDoList{
     constructor(){
         this.todoList = [];
     }
-    newTodoItem(values){
-        let item = new TodoItem(...values);
-        this.todoList.push(item);
+    newTodoProject(value){
+        const duplicateCheck = this.todoList.findIndex(item => item.project === value);
+        if(duplicateCheck > -1){
+            alert(`'${value}' Already Exists. Cancelling Operation`);
+            return;
+        }
+        const newProject = {"project": value,
+                            "tasks": []};
+        this.todoList.push(newProject);
     }
-    get allTodoItems(){
+    newTodoItem(project, valueArray){
+        const targetProject = this.todoList.find(item=> item.project === project);
+        if(typeof(targetProject) === 'undefined'){
+            alert(`${project} Doesn't Exist. Cancelling Operation`);
+            return;
+        }
+        const duplicateCheck = targetProject.tasks.findIndex(item => item.title === valueArray[0]);
+        if(duplicateCheck > -1){
+            alert(`'${valueArray[0]}' Already Exists. Cancelling Operation`);
+            return;
+        }
+        targetProject.tasks.push(new TodoItem(...valueArray));
+    }
+    getAllProjects(){
         return this.todoList;
     }
-    getTodoItems(Project){
-        let requestedItems = [];
-        this.todoList.forEach((item)=>{
-            if(item.project===Project)
-                requestedItems.push(item);
-        });
-        return requestedItems;
+    removeItem(project, value){
+        const targetProject = this.todoList.find(item=> item.project === project);
+        if(typeof(targetProject) === 'undefined'){
+            alert(`${project} Doesn't Exist. Cancelling Operation`);
+            return;
+        }
+        const targetIndex = targetProject.tasks.findIndex(item => item.title === value);
+        if(targetIndex === -1){
+            alert('Title Not Found')
+            return;
+        }
+        targetProject.tasks.splice(targetIndex,1);
+    }
+    removeProject(value){
+        const targetProjectIndex = this.todoList.findIndex(item=> item.project === value);
+        if(targetProjectIndex === -1){
+            alert("Project Not Found")
+            return;
+        }
+        else if(this.todoList[targetProjectIndex].tasks.length){
+            const confirmation = confirm("Project Has Existing Tasks. Confirm Deletion of Project");
+            if(!(confirmation))
+                return;
+        }    
+        this.todoList.splice(targetProjectIndex,1);
     }
 }
 
