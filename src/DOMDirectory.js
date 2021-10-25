4795dd3864d257d663cbe521a0cce6a00c11207b
Raza Hassan <bilgramiraza@yahoo.com>
2021-10-25 06:30:07 +0530

    File holding DOM elements Details

---
+++ src/DOMDirectory.js
@@ -0,0 +1,18 @@
+const DirectoryForm = ()=>{
+    return {
+        project: document.querySelector("#project"),
+        taskTitle: document.querySelector("#title"),
+        taskDiscription: document.querySelector("#discription"),
+        taskDueDate: document.querySelector("#due-date"),
+        taskPriority: document.querySelector("#priority"),
+        taskDone: document.querySelector("#done"),
+    };
+};
+const DirectoryDisplay = ()=>{
+    return {
+        formModal: document.querySelector("form"),
+        taskForm: document.querySelector(".itemFormModal"),
+        projectForm: document.querySelector(".projectFormModal"),
+    };
+};
+export {DirectoryForm, DirectoryDisplay};
