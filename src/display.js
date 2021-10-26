2834899476ebba17292d2083a2ddb655ac7854c5
Raza Hassan <bilgramiraza@yahoo.com>
2021-10-26 10:32:16 +0530

    Added UI Building Functions for projects/tasks

--- src/display.js
+++ src/display.js
@@ -1,38 +1,53 @@
 import DOMBuilder from "./DOMBuilder";
 import pageLayout from "./pageLayout";
+import { DirectoryDisplay } from './DOMDirectory';
 
 
-function basePageBuilder() {
+const basePageBuilder = ()=> {
     pageLayout.forEach((obj)=>{
         DOMBuilder(obj);
     });
 
 }
+const buildProjects = (todoList)=>{
+    const projectTitles = todoList.getAllProjects();
+    const projectList = document.querySelector(".projectList");
+    projectTitles.forEach((title)=>{
+        const Project = {
+            element: "div",
+            className: ["project"],
+            textContent: title,
+          };
+        DOMBuilder(Project, projectList);
+    });
+};
 
-// Directory.displayProjects.addEventListener('click',()=>{
-//     console.log("display Projects");
-//     // console.table(todoList.getAllProjects());
-// });
+const buildTasks = (todoList, projectTitle)=>{
+    const taskTitles = todoList.getAllTaskTitles(projectTitle);
+    const taskList = document.querySelector(".taskGrid");
+    //TODO: Error Handling
+    //TODO: Sorting tasks by priority before displaying
+    taskTitles.forEach((title)=>{
+        const Task = {
+            element: "div",
+            className: ["task"],
+            textContent: title,
+          };
+        DOMBuilder(Task, taskList);
+    });
+};
 
+const displayTask = (todoList, projectTitle, taskTitle)=>{
+    const DirDisplay = DirectoryDisplay();
+    const task = todoList.getTask(projectTitle, taskTitle);
+    DirDisplay.taskTitle.textContent = task[0];
+    DirDisplay.taskDiscription.textContent = task[4];
+    DirDisplay.taskDueDate.textContent = task[1];
+    DirDisplay.taskPriority.textContent = task[2];
+    DirDisplay.taskDone.textContent = task[3];
+}
 
-// tasksDisplayBtn.addEventListener('click',()=>{
-//     const project = prompt('Enter Project Title');
-//     console.table(todoList.getAllTasks(project));
-// });
-
-
-// addProjectBtn.addEventListener('click', ()=>{
-//     const project = prompt('Enter Project Title');
-//     todoList.newTodoProject(project);
-// });
-
-
-// addItemBtn.addEventListener('click', ()=>{
-//     const project = prompt('Enter Project Title');
-//     const item = ["Test", new Date().toISOString(), 2, false, "Testing"];
-//     todoList.newTodoItem(project,item);
-// });
-
+export {basePageBuilder, buildProjects, buildTasks, displayTask};
 
 // toggleDoneBtn.addEventListener('click', ()=>{
 //     const project = prompt('Enter Project Title');
@@ -71,5 +86,3 @@ function basePageBuilder() {
 
 
 
-
-export default basePageBuilder;
